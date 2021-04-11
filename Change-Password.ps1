3f917e0861c222083ee76cda25141e7c9d293320
Robert Przybylski <robert.przybylski@predicagroup.com>
2021-04-11 19:25:08 +0200

    Change-Password: script to change password from command line

---
+++ Change-Password.ps1
@@ -0,0 +1,4 @@
+﻿$oldPassword = (Get-Credential 'OldPassword').GetNetworkCredential().Password
+$newPassword = (Get-Credential 'NewPassword').GetNetworkCredential().Password
+$user = [adsi]"WinNT://$Env:UserDomain/$Env:UserName,user"
+$user.ChangePassword($oldPassword, $newPassword)
